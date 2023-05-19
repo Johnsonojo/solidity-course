10be98cd292f66220cb2b62038abe86041c13234
Johnsonojo <johnsonojo@gmail.com>
2023-05-19 22:10:41 +0100

    added comments lesson

---
+++ 01-comment.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Comments {
+    // Comments in solidity are a way to document our code
+    // This is a single line comment
+    /*
+        This is a multi line comment
+        I'm learning solidity programming so that I can be skilled enough to take advantage of the various opportunity in the 
+        the DeFi space. I believe that by the power, grace and wisdom of God, I will achieve my goal of becoming highly proficient
+        in the solidity programming language
+
+        I have made up my mind to commit to this on a daily basis irrespective of my schedule.
+    */
+}
