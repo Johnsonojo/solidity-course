f8af6e2a6e998660bb4fde3af8c4c7e058234457
Johnsonojo <johnsonojo@gmail.com>
2023-05-26 20:00:04 +0100

    updated lesson 2 of section 5

--- section-05/03-strings.sol
+++ section-05/03-strings.sol
@@ -12,21 +12,15 @@ contract LearnStrings {
     }
 
     // memory is used to store the variable in memory and not in storage. This is because the function is a view function and it does not change the state of the contract.
-
     function greet(string memory _name) public {
         greeting = _name;
     }
 
     // Strings Vs Bytes
-
     // Strings are stored as UTF-8 encoded data. UTF-8 is a variable length encoding mechanism for Unicode. UTF-8 is backwards compatible with ASCII. UTF-8 is the most commonly used encoding mechanism for web applications.
-
     // Bytes are stored as binary data. They are stored as a fixed length sequence of bytes. Bytes are used to store raw data. They are used to store data that is not meant to be interpreted as text.
-
     // Bytes are more efficient than strings. Strings are more expensive than bytes.
-
     // Bytes are used to store raw data. Strings are used to store text.
-
     // Bytes are used to store data that is not meant to be interpreted as text. Strings are used to store text that is meant to be interpreted as text.
 
     // A function to return the length of the string
