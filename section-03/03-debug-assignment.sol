93a944c639d0b6651141131dd50b6c1cc6d204b0
Johnsonojo <johnsonojo@gmail.com>
2023-05-26 19:55:21 +0100

    update lesson 3 of section 3

--- section-03/03-debug-assignment.sol
+++ section-03/03-debug-assignment.sol
@@ -3,16 +3,11 @@ pragma solidity >=0.7.0 <0.9.0;
 
 /* 
 You just land your new blockchain job to build smart contracts for an awesome DAO catching fire!
-
 Immediately, your boss throws at your a scrip of code tons of bugs in it. She says, 
-
 'Hey you! Fix this code and get this smart contract running and help save our DApp from crashing!'
-
 The clock is ticking and this is the perfect opportunity to prove your skills and show what you are made of.
-
 Your assignment is to fix the code below full of syntax errors and get this contract successfully deployed on 
 the Ethereum network. 
-
 Good luck! 
 */
 
