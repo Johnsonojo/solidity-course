b32c172a0773057a6ed21df5f2b06aa0c90db1ee
Johnsonojo <johnsonojo@gmail.com>
2023-05-26 20:02:36 +0100

    updated lesson 11 of section 6

--- section-06/11-nested-mapping-exercise.sol
+++ section-06/11-nested-mapping-exercise.sol
@@ -2,9 +2,7 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 // Observe the following contract which has functionality to remove a spender address paired with an owner address:
-
 // Your assignment is to build out the rest of the contract functionality. Create a Contract, using nested mapping, to pair two contract addresses together (an address owner and an address spender) with a value of integers to keep track of their index.
-
 // One you have successfully set up the map, add additional contract functionality so that the contract can pair up new addresses together (owner and spender) as well as retrieve their mapping value.
 
 contract Allowance {
